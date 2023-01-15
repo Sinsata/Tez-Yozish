49015d51cfadeb43a5671f1afd66250a7d7639f4
Sinsata <raupovhojiboy@gmail.com>
2023-01-16 00:27:10 +0500

    new wersion

--- src/js/snow.js
+++ src/js/snow.js
@@ -1,12 +1,1 @@
-var snow_text = '';
-
-for(var i=0; i<200; i++){
-  snow_text+=`<div class="snow"></div>`;
-}
-
-document.getElementById('snow').innerHTML=snow_text;
-
-window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-LLWL5N9CSM');
+for(var snow_text="",i=0;i<200;i++)snow_text+='<div class="snow"></div>';function gtag(){dataLayer.push(arguments)}document.getElementById("snow").innerHTML=snow_text,window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-LLWL5N9CSM");
