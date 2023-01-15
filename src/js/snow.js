ece2a55f10d0341624fc7b19824c1b8d951e2fd6
Sinsata <raupovhojiboy@gmail.com>
2023-01-15 19:33:15 +0500

    .

---
+++ src/js/snow.js
@@ -0,0 +1,12 @@
+var snow_text = '';
+
+for(var i=0; i<200; i++){
+  snow_text+=`<div class="snow"></div>`;
+}
+
+document.getElementById('snow').innerHTML=snow_text;
+
+window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-LLWL5N9CSM');
